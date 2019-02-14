7815d22d9b7be9bf7886e838bf86eecd33311c64
Zhenfang Wei <kopkop@gmail.com>
2019-02-14 20:03:23 +0100

    Added 2 easy solution

---
+++ 21.merge-two-sorted-lists.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+#
+# [21] 合并两个有序链表
+#
+# https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+#
+# algorithms
+# Easy (51.80%)
+# Total Accepted:    40.2K
+# Total Submissions: 77.5K
+# Testcase Example:  '[1,2,4]\n[1,3,4]'
+#
+# 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+# 
+# 示例：
+# 
+# 输入：1->2->4, 1->3->4
+# 输出：1->1->2->3->4->4
+# 
+# 
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            res = ListNode(l1.val)
+            l1 = l1.next
+        else:
+            res = ListNode(l2.val)
+            l2 = l2.next
+        current = res
+        while l1 or l2:
+            if not l1:
+                res.next = l2
+                break
+            elif not l2:
+                res.next = l1
+                break
+            elif l1.val < l2.val:
+                res.next = ListNode(l1.val)
+                l1 = l1.next
+            else:
+                res.next = ListNode(l2.val)
+                l2 = l2.next
+            res = res.next
+        return current
+
+        
